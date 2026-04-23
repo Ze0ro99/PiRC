26b03d308f1494ce9080aa6ec7fc00d9545b88f4
Ze0ro99 <kamelkadah910@gmail.com>
2026-04-23 17:48:42 +0300

    chore(omni-master): autonomous pipeline complete, full branch sync, post-quantum layer verified

--- Omni_Sovereign_Architecture/Compiled_Ecosystem/pirc_241/src/lib.rs
+++ Omni_Sovereign_Architecture/Compiled_Ecosystem/pirc_241/src/lib.rs
@@ -1,6 +1,7 @@
 #![forbid(unsafe_code)]
 #![forbid(unsafe_code)]
 #![forbid(unsafe_code)]
+#![forbid(unsafe_code)]
 #![no_std]
 #![forbid(unsafe_code)]
 use soroban_sdk::{contract, contractimpl, Address, Env};
--- rwa_verify/src/lib.rs
+++ rwa_verify/src/lib.rs
@@ -7,6 +7,7 @@
 #![forbid(unsafe_code)]
 #![forbid(unsafe_code)]
 #![forbid(unsafe_code)]
+#![forbid(unsafe_code)]
 mod pirc_config;
 #![no_std]
 
